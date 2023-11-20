a4ac07f23bf8397a27a38c073eb6366fb95b04c0
Van Phat <vanphathutechit@gmail.com>
2023-11-20 15:29:10 +0000

    update logger

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ from logging import basicConfig, getLogger, DEBUG
 class Logger:
     def __init__(self) -> None:
         basicConfig(format='%(asctime)s %(levelname)s | %(message)s', datefmt='%H:%M:%S')
-        self.__logger = getLogger()
+        self.__logger = getLogger("main")
         self.__logger.setLevel(DEBUG)
 
     def error(self, text: str):
